ec937c44f5bb0d232adfd232a180c035dc44bf33
Chaleira <pedromchaleira@gmail.com>
2025-05-13 18:43:51 +0100

    added missing route to block

--- matcha-express_new/src/routes/blockRoute.ts
+++ matcha-express_new/src/routes/blockRoute.ts
@@ -8,6 +8,7 @@ const router = Router();
 router.post("/create", validateQueryParams(["blocked_id"], ["blocked_id"]), validateBodyParams(null), catchAsync(blockController.createBlock));
 router.post("/delete", validateQueryParams(["blocked_id"], ["blocked_id"]), validateBodyParams(null), catchAsync(blockController.deleteBlock));
 
+router.get('/get', validateQueryParams(["user_id"], ["user_id"]), validateBodyParams(null), catchAsync(blockController.getBlock));
 router.get("/by-blocker", validateQueryParams(["blocker_id"], ["blocker_id"]), validateBodyParams(null), catchAsync(blockController.getBlocksByBlocker));
 router.get("/by-blocked", validateQueryParams(["blocked_id"], ["blocked_id"]), validateBodyParams(null), catchAsync(blockController.getBlocksByBlocked));
 
