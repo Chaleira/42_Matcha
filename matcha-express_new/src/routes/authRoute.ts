47364cca08a00a493bdfd0b965b3e4c70d5aeb61
Chaleira <pedromchaleira@gmail.com>
2025-05-15 17:38:25 +0100

    added progile variables to register api

--- matcha-express_new/src/routes/authRoute.ts
+++ matcha-express_new/src/routes/authRoute.ts
@@ -3,13 +3,14 @@ import { authController } from "../controller/authController";
 import { catchAsync } from "../utils/catchAsync";
 import { validateBodyParams, validateQueryParams } from "../middleware/validationMiddleware";
 
-const allowedRegisterParams = ["username", "email", "first_name", "last_name", "password"];
+const allowedRegisterParams = ["username", "email", "first_name", "last_name", "password", "bio", "age", "tags", "gender", "sexual_preference", "pictures", "avatar", "fame_score", "latitude", "longitude"];
+const requiredRegisterParams = ["username", "email", "first_name", "last_name", "password"]
 const allowedLoginParams = ["username", "password"];
 
 const router = Router();
 
-router.post("/register", validateQueryParams(null), validateBodyParams(allowedRegisterParams), catchAsync(authController.register));
-router.post("/login", validateQueryParams(null), validateBodyParams(allowedLoginParams), catchAsync(authController.login));
+router.post("/register", validateQueryParams(null), validateBodyParams(allowedRegisterParams, requiredRegisterParams), catchAsync(authController.register));
+router.post("/login", validateQueryParams(null), validateBodyParams(allowedLoginParams, allowedLoginParams), catchAsync(authController.login));
 
 router.get("/verify-email", validateQueryParams(["token"]), validateBodyParams(null), catchAsync(authController.verifyEmail));
 
